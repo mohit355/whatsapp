3f17c816a15c406a21b5e85363f447efc6d2ed80
mohit355 <mohit_bt18@iiitkalyani.ac.in>
2020-11-01 01:13:25 +0530

    adding firebase.js file

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDAfeTQAAGwQOZ_wUzZe0vxCehNHMmNrH0",
+  authDomain: "whatsapp-chitchat.firebaseapp.com",
+  databaseURL: "https://whatsapp-chitchat.firebaseio.com",
+  projectId: "whatsapp-chitchat",
+  storageBucket: "whatsapp-chitchat.appspot.com",
+  messagingSenderId: "645675905049",
+  appId: "1:645675905049:web:364e3bc847b5a5a8637a88",
+  measurementId: "G-0M1CL01KMC",
+};
