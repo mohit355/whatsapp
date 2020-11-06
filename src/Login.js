78901be0959501606018b73d46b80e37f859674f
mohit355 <mohit_bt18@iiitkalyani.ac.in>
2020-11-07 01:49:32 +0530

    adding google auth

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,17 @@
 // import { Button } from "@material-ui/core";
 import React from "react";
 import { Button } from "@material-ui/core";
-
+import { auth, provider } from "./firebase";
 import "./Login.css";
+
 function Login() {
-  const signIn = () => {};
+  const signIn = () => {
+    auth
+      .signInWithPopup(provider)
+      .then((result) => console.log(result))
+      .catch((error) => alert(error.message));
+  };
+
   return (
     <div className="login">
       <div className="login__container">
